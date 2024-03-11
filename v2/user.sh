4d9139acfdeef3ca8e2063f338fd9b9b0366cdaa
kesokaj <ostling.simon@gmail.com>
2024-03-11 14:08:31 +0000

    fee8uif0okohb5kabohsh9aeGahw4ohz

--- v2/user.sh
+++ v2/user.sh
@@ -4,4 +4,4 @@ mkdir -p $XDG_RUNTIME_DIR
 mkdir -p $WORKSPACE
 
 dockerd-rootless.sh &
-code-server --bind-addr=0.0.0.0:8080 --disable-telemetry --auth=none --user-data-dir=$HOME/.config ${WORKSPACE}
+code-server --bind-addr=0.0.0.0:3000 --disable-telemetry --auth=none --user-data-dir=$HOME/.config ${WORKSPACE}
